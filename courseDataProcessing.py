3dc4992d046bac896c2532bb383cc5d98fb1eb02
jsutantio <jessica.sutantio@students.olin.edu>
2015-03-30 14:27:23 -0400

    New python document

---
+++ courseDataProcessing.py
@@ -0,0 +1,10 @@
+# On the Right Course
+# Aditi Joshi and Jessica Sutantio
+# Software Design Final Project
+
+""" From all of the Olin registaration data, we will be creating a visualization 
+displaying the most commong courses taken during a student's Olin career. 
+Users will be able to filter the data so that they may visualize the informaiton 
+based on major and/ or semester.
+"""
+
