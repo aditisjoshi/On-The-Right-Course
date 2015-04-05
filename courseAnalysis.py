a5f9b413a4d840d5e16d4e440a6b075b2a6f7b3b
Aditi <aditisatishjoshi@gmail.com>
2015-04-05 16:44:08 -0400

    Implemented pandas and created data frame.

--- courseAnalysis.py
+++ courseAnalysis.py
@@ -17,7 +17,41 @@ import numpy as np
 # Name of data file
 file_name = 'course_enrollments_2002-2014spring_anonymized.csv'
 
-def get_data_as_lists(file_name):
+
+def course_time(academicStatus,academicYear):
+    """
+    academicStatus: list denoting freshman, sophomore, junior, and senior status
+    academicYear: list denoting semester based off year courses are taken
+    returns: course_semester_taken, which is a list of tuples with courseNumbers 
+    being paired with the semester they are taken
+    """
+
+    # Calculating semester and paring courses with semester
+    course_semester_taken = []
+    for i in range(len(academicStatus)):
+        if (academicStatus[i]=='TF') or (academicStatus[i]=='FF'):
+            course_semester_taken.append(1.0)
+        elif academicStatus[i]=='FR':
+            course_semester_taken.append(1.5)
+        elif academicStatus[i]=='SO' and ('FA' in academicYear[i]):
+            course_semester_taken.append(2.0)
+        elif academicStatus[i]=='SO' and ('SP' in academicYear[i]):
+            course_semester_taken.append(2.5)
+        elif academicStatus[i]=='JR' and ('FA' in academicYear[i]):
+            course_semester_taken.append(3.0)
+        elif academicStatus[i]=='JR' and ('SP' in academicYear[i]):
+            course_semester_taken.append(3.5)
+        elif academicStatus[i]=='SR' and ('FA' in academicYear[i]):
+            course_semester_taken.append(4.0)
+        elif academicStatus[i]=='SR' and ('SP' in academicYear[i]):
+            course_semester_taken.append(4.5)
+        else:
+            course_semester_taken.append(np.nan)
+
+    return course_semester_taken
+
+
+def get_df(file_name):
     """
     file_name: csv file that contains the course data
     separates the data from the file into its columns, each column is a list
@@ -52,12 +86,11 @@ def get_data_as_lists(file_name):
             courseTitle.append(courseData[10] + courseData[11]) # includes subtitle
             professor.append(courseData[11])
 
-    return gradStatus, gradYear, ID, academicYear, gender, academicStatus, major, courseNum, courseSect, courseTitle, professor
+    semester = course_time(academicStatus,academicYear)
+
+    df = pd.DataFrame({'gradStatus': gradStatus, 'gradYear': gradYear, 'ID': ID, 'academicYear': academicYear, 'gender': gender, 'semester': semester, 'major': major, 'courseNum': courseNum, 'courseSect': courseSect, 'courseTitle': courseTitle, 'professor': professor})
+
+    return df
 
 
-df = pd.DataFrame({ 'gradStatus' : 1.,
-                      'B' : pd.Timestamp('20130102'),
-                      'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
-                     'D' : np.array([3] * 4,dtype='int32'),
-                     'E' : pd.Categorical(["test","train","test","train"]),
-                     'F' : 'foo' })
+print get_df(file_name)
