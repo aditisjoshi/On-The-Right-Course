ead3ed5d5e4c3b9a9440019f00240c844769a455
Aditi <aditisatishjoshi@gmail.com>
2015-04-16 13:43:20 -0400

    adding code review documents

--- getmajor.py
+++ getmajor.py
@@ -1,15 +1,21 @@
-@app.route('/logworkout', methods=['GET','POST'])
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/index', methods=['GET','POST'])
 def post_log_workout(): 
     error=""
     if request.method == 'POST':   
         major = (request.form.get['major'])
         if major is not None:       
-            workout = Workout(major=major)
-            db.session.add(workout)
             db.session.commit() 
             # you can redirect to home page on successful commit. or anywhere else
-            return redirect()       
+            return hello world     
         else:
-            error="Error, your log is incomplete! Please check and submit it again!")
+            return hello world
 
     return render_template("index.html", error=error)
+
+
+if __name__ == "__main__":
+    app.run()
