4dc6080cd8ec2c58e40517fdca8b922bd88573d1
jsutantio <jessica.sutantio@students.olin.edu>
2015-04-16 14:52:35 -0400

    Transferred functions. Code not fully prepared.

--- OnTheRightCourse_dataAnalysis.py
+++ OnTheRightCourse_dataAnalysis.py
@@ -18,40 +18,6 @@ import numpy as np
 # Name of data file
 file_name = 'course_enrollments_2002-2014spring_anonymized.csv'
 
-
-def course_time(academicStatus,academicYear):
-    """
-    academicStatus: list denoting freshman, sophomore, junior, and senior status
-    academicYear: list denoting semester based off year courses are taken
-    returns: course_semester_taken, which is a list of tuples with courseNumbers 
-    being paired with the semester they are taken
-    """
-
-    # Calculating semester and paring courses with semester
-    course_semester_taken = []
-    for i in range(len(academicStatus)):
-        if (academicStatus[i]=='TF') or (academicStatus[i]=='FF'):
-            course_semester_taken.append(1.0)
-        elif academicStatus[i]=='FR':
-            course_semester_taken.append(1.5)
-        elif academicStatus[i]=='SO' and ('FA' in academicYear[i]):
-            course_semester_taken.append(2.0)
-        elif academicStatus[i]=='SO' and ('SP' in academicYear[i]):
-            course_semester_taken.append(2.5)
-        elif academicStatus[i]=='JR' and ('FA' in academicYear[i]):
-            course_semester_taken.append(3.0)
-        elif academicStatus[i]=='JR' and ('SP' in academicYear[i]):
-            course_semester_taken.append(3.5)
-        elif academicStatus[i]=='SR' and ('FA' in academicYear[i]):
-            course_semester_taken.append(4.0)
-        elif academicStatus[i]=='SR' and ('SP' in academicYear[i]):
-            course_semester_taken.append(4.5)
-        else:
-            course_semester_taken.append(np.nan)
-
-    return course_semester_taken
-
-
 def get_df(file_name):
     """
     file_name: csv file that contains the course data
@@ -100,22 +66,114 @@ class CourseDF(objet):
 
 	
 	def dataCleaning(df):
+		semLabel(df)
+		majorAssignment(df)
+		
 
+		def semLabel(academicStatus,academicYear):
+			"""
+		    academicStatus: list denoting freshman, sophomore, junior, and senior status
+		    academicYear: list denoting semester based off year courses are taken
+		    returns: course_semester_taken, which is a list of tuples with courseNumbers 
+		    being paired with the semester they are taken
+		    """
 
-		def semLabel(df):
+		    # Calculating semester and paring courses with semester
+		    course_semester_taken = []
+		    for i in range(len(academicStatus)):
+		        if (academicStatus[i]=='TF') or (academicStatus[i]=='FF'):
+		            course_semester_taken.append(1.0)
+		        elif academicStatus[i]=='FR':
+		            course_semester_taken.append(1.5)
+		        elif academicStatus[i]=='SO' and ('FA' in academicYear[i]):
+		            course_semester_taken.append(2.0)
+		        elif academicStatus[i]=='SO' and ('SP' in academicYear[i]):
+		            course_semester_taken.append(2.5)
+		        elif academicStatus[i]=='JR' and ('FA' in academicYear[i]):
+		            course_semester_taken.append(3.0)
+		        elif academicStatus[i]=='JR' and ('SP' in academicYear[i]):
+		            course_semester_taken.append(3.5)
+		        elif academicStatus[i]=='SR' and ('FA' in academicYear[i]):
+		            course_semester_taken.append(4.0)
+		        elif academicStatus[i]=='SR' and ('SP' in academicYear[i]):
+		            course_semester_taken.append(4.5)
+		        else:
+		            course_semester_taken.append(np.nan)
+
+		    return course_semester_taken
 
 
 		def majorAssignment(df):
+						"""
+		    this method takes in each student's ID and their major. If their major is 
+		    undefined at any point, the function will take in the first major it finds
+		    for that ID 
+
+		    MAJORS SYNTAX
+		     ['Undeclared              ' 
+		     'Mechanical Engineering  '
+		     'Engineering             '
+		     'Undeclared              Systems                 '
+		     'Engineering             Systems                 '
+		     'Undeclared              Computing               '
+		     'Engineering             Computing               '
+		     'Undeclared              Materials Science       '
+		     'Mechanical Engineering  Materials Science       '
+		     'Engineering             Materials Science       '
+		     'Mechanical Engineering  Computing               '
+		     'Undeclared              Bioengineering          '
+		     'Engineering             Bioengineering          '
+		     "Electr'l & Computer Engr"
+		     'Undeclared              Self Designed           '
+		     'Engineering             Self Designed           '
+		     'Mechanical Engineering  Bioengineering          '
+		     "Electr'l & Computer EngrComputing               "
+		     "Electr'l & Computer EngrSystems                 "
+		     'Mechanical Engineering  Systems                 '
+		     'Mechanical Engineering  Self Designed           '
+		     "Electr'l & Computer EngrSelf Designed           "
+		     'Undeclared              Design                  '
+		     'Mechanical Engineering  Design                  '
+		     'Engineering             Design                  '
+		     'Undeclared              Robotics                '
+		     'Mechanical Engineering  Robotics                '
+		     'Engineering             Robotics                '
+		     'Exchange Student        Computing               '
+		     "Electr'l & Computer EngrRobotics                "]
+		    """
+
+		    # all majors that they can list from: ME, ECE, E:C, E:Robo, E:Bio, E:MatSci, E:Design, E:Systems
+		    major_convert = {'ME': 'Mechanical Engineering  ', 'ECE': "Electr'l & Computer Engr", 'E:C': 'Engineering             Computing               ', 'E:Robo': 'Engineering             Robotics                ', 'E:Bio': 'Engineering             Bioengineering          ', 'E:MatSci': 'Engineering             Materials Science       ', 'E:Design': 'Engineering             Design                  ', 'E:Systems': 'Engineering             Systems                 '}
+		    major = major_convert[major]
+
+		    # finds all the unique ids and all ids
+		    uniqueIDs = df.ID.unique()
+		    uniquemajors = df.major.unique()
+
+		    # find the start and end indices of the unique IDs 
+		    for idNum in uniqueIDs:
+		        IDindex = df[df['ID']==idNum].index.tolist()
+		        startID = IDindex[0]
+		        endID = IDindex[-1]
+		        # find the last updated major of the student
+		        latestMajor = df['major'][endID]
+		        # change all previous majors to be the same as last updated major
+		        df.loc[startID:endID+1, 'major'] = latestMajor
+
+        	return df
 
 
 	def filter(self, sem=none, major=none):
 
 
 		def semFilter(self,df, sem):
+			semCourses = 
 
 
 		def majorFilter(self,df, major):
+			majorDF = self.df[self.df.major == major]
 
+			return majorDF
 
 
 
