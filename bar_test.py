190c98342fb2f6efe17f2f5879ee3c3c1ff3edfb
jsutantio <jessica.sutantio@students.olin.edu>
2015-04-28 23:02:10 -0400

    Added test file for plotting

---
+++ bar_test.py
@@ -0,0 +1,61 @@
+"""
+Simple demo of a horizontal bar chart.
+"""
+# import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+
+plt.barh(y_pos, performance, align='center', alpha=.75)
+
+# Adds the appropriate labeling of data points
+plt.yticks(y_pos, people)
+
+# Makes the axes labels
+# plt.xlabel('Performance')
+# plt.title('How fast do you want to go today?')
+
+plt.show()
+
+"""
+For putting labels within the bars
+http://matplotlib.org/examples/pylab_examples/barchart_demo2.html
+
+# Lastly, write in the ranking inside each bar to aid in interpretation
+for rect in rects:
+    # Rectangle widths are already integer-valued but are floating
+    # type, so it helps to remove the trailing decimal point and 0 by
+    # converting width to int type
+    width = int(rect.get_width())
+
+    # Figure out what the last digit (width modulo 10) so we can add
+    # the appropriate numerical suffix (e.g., 1st, 2nd, 3rd, etc)
+    lastDigit = width % 10
+    # Note that 11, 12, and 13 are special cases
+    if (width == 11) or (width == 12) or (width == 13):
+        suffix = 'th'
+    else:
+        suffix = suffixes[lastDigit]
+
+    rankStr = str(width) + suffix
+    if (width < 5):        # The bars aren't wide enough to print the ranking inside
+        xloc = width + 1   # Shift the text to the right side of the right edge
+        clr = 'black'      # Black against white background
+        align = 'left'
+    else:
+        xloc = 0.98*width  # Shift the text to the left side of the right edge
+        clr = 'white'      # White on magenta
+        align = 'right'
+
+    # Center the text vertically in the bar
+    yloc = rect.get_y()+rect.get_height()/2.0
+    ax1.text(xloc, yloc, rankStr, horizontalalignment=align,
+            verticalalignment='center', color=clr, weight='bold')
+"""
