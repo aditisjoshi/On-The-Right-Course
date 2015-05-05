86b1ca1d87c1e38fa61ffab3787f2b74ba69003b
Aditi <aditisatishjoshi@gmail.com>
2015-05-05 02:14:10 -0400

    fixed not problem

--- app/views.py
+++ app/views.py
@@ -13,20 +13,27 @@ def major_filtering():
 
 @app.route('/filter', methods=['POST'])
 def filter():
+    # getting user input 
     major = (request.form['major'])
     semester = (request.form['semester'])
+
+    # converting the major input to what can be used in the dictionary
     major_convert = {'ME': 'Mechanical Engineering  ', 'ECE': "Electr'l & Computer Engr", 'E:C': 'Engineering             Computing               ', 'E:Robo': 'Engineering             Robotics                ', 'E:Bio': 'Engineering             Bioengineering          ', 'E:MatSci': 'Engineering             Materials Science       ', 'E:Design': 'Engineering             Design                  ', 'E:Systems': 'Engineering             Systems                 ', 'None': ''}
     major = major_convert[major]
+
+    # getting the picture file names
     path = 'static/'
     if semester is None:
-        image_name = path + 'plot' + semester + '_' + major + '.png'
-    else:
         image_name = []
         semesters = [1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5]
         for semester in semesters:
             image_name_next = path + 'plot' + str(semester) + '_' + major + '.png'
             image_name.append(image_name_next)
         print image_name
+    else:
+        image_name = path + 'plot' + semester + '_' + major + '.png'
+        print image_name
+
     return render_template('filter.html', sem=semester, major=major, image_name=image_name)
 
 
